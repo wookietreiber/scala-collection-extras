2c55e3a281823272946c037d2c0de56160d6d96e
Christian Krause <kizkizzbangbang@googlemail.com>
2012-07-19 14:54:05 +0200

    now have GenTraversableMixer

--- Mixer.scala
+++ Mixer.scala
@@ -3,6 +3,8 @@ package scalax.util
 import language.higherKinds
 
 import scala.annotation.tailrec
+import scala.collection.GenTraversable
+import scala.collection.generic.CanBuildFrom
 import scala.collection.parallel.immutable.ParVector
 import scala.util.Random._
 
@@ -10,7 +12,30 @@ trait Mixer[M[_]] extends Sorter[M] {
   def shuffle[A](xs: M[A]): M[A]
 }
 
-object Mixer {
+trait MixerLow {
+  implicit def GenTraversableMixer[CC[X] <: GenTraversable[X]]: Mixer[CC] = new Mixer[CC] {
+    override def merge[A:Ordering](xs: CC[A], y: A) = {
+      val (ps,ss) = xs.partition(implicitly[Ordering[A]].lt(_,y))
+      val builder = xs.genericBuilder[A]
+      builder ++= ps.seq
+      builder  += y
+      builder ++= ss.seq
+      builder.result.asInstanceOf[CC[A]]
+    }
+
+    @tailrec
+    override def mergeAll[A:Ordering](xs: CC[A], ys: CC[A]) =
+      if (ys.isEmpty) xs else mergeAll(merge(xs,ys.head), ys.tail.asInstanceOf[CC[A]])
+
+    override def sort[A:Ordering](xs: CC[A]) =
+      xs.aggregate(xs.genericBuilder[A].result.asInstanceOf[CC[A]])(merge, mergeAll)
+
+    override def shuffle[A](xs: CC[A]): CC[A] =
+      sortBy(xs) { _ ⇒ nextLong }
+  }
+}
+
+object Mixer extends MixerLow {
   implicit val ParVectorMixer: Mixer[ParVector] = new Mixer[ParVector] {
     override def merge[A:Ordering](xs: ParVector[A], y: A): ParVector[A] = {
       val (ps,ss) = xs.partition(implicitly[Ordering[A]].lt(_,y))
@@ -25,15 +50,8 @@ object Mixer {
     override def mergeAll[A:Ordering](xs: ParVector[A], ys: ParVector[A]): ParVector[A] =
       if (ys.isEmpty) xs else mergeAll(merge(xs,ys.head), ys.tail)
 
-    override def sort[A:Ordering](xs: ParVector[A]): ParVector[A] = {
-      xs.aggregate(xs.companion.empty[A])(
-        (xs,y) ⇒ {
-          val (ps,ss) = xs.partition(implicitly[Ordering[A]].lt(_,y))
-          ps ++ xs.companion(y) ++ ss
-        },
-        mergeAll
-      )
-    }
+    override def sort[A:Ordering](xs: ParVector[A]): ParVector[A] =
+      xs.aggregate(xs.companion.empty[A])(merge, mergeAll)
 
     override def shuffle[A](xs: ParVector[A]): ParVector[A] =
       sortBy(xs) { _ ⇒ nextLong }
