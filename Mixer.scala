67ddc9e6c6bd1442cb6da37e8edc400da0300ff1
Christian Krause <kizkizzbangbang@googlemail.com>
2012-08-08 09:27:39 +0200

    SeqMixer to use more efficient sorting

--- Mixer.scala
+++ Mixer.scala
@@ -27,9 +27,20 @@ trait Mixer[M[_]] {
   def choosePair[A](xs: M[A]): Pair[A,A]
 }
 
-trait MixerLow {
+trait MixerLow0 {
   implicit def GenTraversableMixer[CC[X] <: GenTraversable[X]](implicit sorter: Sorter[CC]): Mixer[CC] = new Mixer[CC] {
     override def shuffle[A](xs: CC[A]): CC[A] = sorter.sortBy(xs) { _ ⇒ Random.nextLong }
+    override def choose[A](xs: CC[A], n: Int): CC[A] = shuffle(xs).take(n).asInstanceOf[CC[A]]
+    override def choosePair[A](xs: CC[A]): Pair[A,A] = {
+      val two = choose(xs, 2).toIndexedSeq
+      Pair(two(0), two(1))
+    }
+  }
+}
+
+trait MixerLow extends MixerLow0 {
+  implicit def SeqMixer[CC[X] <: Seq[X]]: Mixer[CC] = new Mixer[CC] {
+    override def shuffle[A](xs: CC[A]): CC[A] = xs.sortBy(_ ⇒ Random.nextLong).asInstanceOf[CC[A]]
     override def choose[A](xs: CC[A], n: Int): CC[A] = shuffle(xs).take(n).asInstanceOf[CC[A]]
     override def choosePair[A](xs: CC[A]): Pair[A,A] = {
       val two = choose(xs, 2).toIndexedSeq
